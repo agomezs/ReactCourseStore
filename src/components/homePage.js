03123b6fb5a9a74ceb8421b728e392b0c0433a70
AJ <Me>
2016-01-20 11:54:58 -0600

    With React-router Link

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,4 +1,8 @@
+"use strict";
+
 var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 var Home = React.createClass({
   render: function () {
@@ -6,6 +10,7 @@ var Home = React.createClass({
         <div className="jumbotron">
           <h1>My body</h1>
           <p>Home component build with react :) </p>
+          <Link to="about" className="btn btn-primary btn-lg">Let's go to about...</Link>
         </div>
       );
   }
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
-
+var Redirect = ReactRouter.Redirect;
 
 var routes = (
 
@@ -13,6 +13,10 @@ var routes = (
       <IndexRoute component={require('./components/homePage')} />
       <Route path="authors" component={require('./components/authors/authorPage')} />
       <Route path="about" component={require('./components/about/aboutPage')} />
+
+      <Redirect from="aboutus" to="about"/>
+
+      <Route path="*" component={require('./components/notFoundPage')} />
     </Route>
 
 );
